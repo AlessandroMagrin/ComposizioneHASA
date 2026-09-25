c566aec050500ab4871bc5407f6df3613e87afa5
AlessandroMagrin <14529937@itisrossi.vi.it>
2026-09-25 15:56:57 +0200

    Update CPunto.cs

--- CPunto.cs
+++ CPunto.cs
@@ -9,27 +9,26 @@ namespace Composizione
         private int _x;
         private int _y;
 
+        /* come richiesta dalla consegna, rimuovo le proprierty, e chiamo col ToString() ogni classe
         public int X
         {
-            get {
-                return _x;
-            }
+            get { return _x; }
             set
             {
                 _x = value;
             }
         }
+
         public int Y
         {
-            get
-            {
-                return _y;
-            }
+            get { return _y; }
             set
             {
                 _y = value;
             }
         }
+        */
+
         public CPunto(){
             X=0;
             Y=0;
@@ -39,6 +38,10 @@ namespace Composizione
             X = x;
             Y = y;
 
+        }
+          public override string ToString()
+        {
+            return ("X:"+_x+"Y:"+_y);
         }
     }
 }
